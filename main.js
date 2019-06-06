7c46572a25a4bd5ee76405a2151b798054f104b3
Javis Johnson <jsj1027@g.rit.edu>
2019-06-05 23:49:45 -0400

    woo

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-const electron = require('electron')
+const { app, BrowserWindow } = require('electron');
 
-function createWindow () {
+function createWindow() {
   let win = new BrowserWindow({
     width: 800,
-    height: 600
-  })
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile('index.html');
 }
+
+app.on('ready', createWindow);
