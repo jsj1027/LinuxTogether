7d200e5026b24070b2ea0289f426f6bcaac1d062
Javis Johnson <jsj1027@g.rit.edu>
2019-06-06 23:20:46 -0400

    Added script control

---
+++ Shell/script.js
@@ -0,0 +1,14 @@
+const { exec } = require('child_process');
+
+function sh(cmd) {
+  return function() {
+    exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        Promise.reject(err);
+      }
+      return { stdout, stderr };
+    });
+  };
+}
+//https://stackoverflow.com/questions/1880198/how-to-execute-shell-command-in-javascript
+// but put main in main.js
