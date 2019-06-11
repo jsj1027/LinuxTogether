c61ed676d05480afb261614d5c0f7a7afbd12943
Javis Johnson <jsj1027@g.rit.edu>
2019-06-11 18:45:18 -0400

    ez fix

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <gtkmm-3.0/gtkmm.h>
 
-// compile with gtklibs   g++ main.cpp -o main.cpp `pkg-config gtkmm-3.0 --cflags --libs`
+// compile with gtklibs   g++ main.cpp -o main `pkg-config gtkmm-3.0 --cflags --libs`
 int main(int argc, char* argv[]) {
     Gtk::Main kit(argc, argv);
 
