6a835223345667e0440f91b9631b1cccb6c55277
Javis Johnson <jsj1027@g.rit.edu>
2019-06-15 12:04:01 -0400

    found a way to get distro name i think may need other checks

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 #include <gtkmm-3.0/gtkmm.h>
 #include <CLI11.hpp>
+#include <sys/utsname.h>
+#include <stdio.h>
+
+int uname(struct utsname *buf);
 
 int main(int argc, char* argv[]) {
+    utsname* hi;
+    uname(hi); 
+    // char* ok = hi->sysname;
+    printf("HI: %s\n%s\n%s\n%s\n%s\n%s\n", hi->sysname, hi->nodename, hi->release, hi->version, hi->machine, hi->domainname);
+    
     Gtk::Main kit(argc, argv);
 
     Gtk::Window window;
 
     Gtk::Main::run(window);
+
 
     return 0;
 }
